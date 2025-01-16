5cd385ae3d4117382921add1614497d6dfad3f33
lukcic <lukcic@int.pl>
2025-01-16 23:36:53 +0100

    python: vpc-stack

--- aws/cdk-python/app.py
+++ aws/cdk-python/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from atexit import register
-from dotenv import load_dotenv
+from dotenv import dotenv_values, load_dotenv
 from stacks.vpc_stack import VpcStack
 from aws_cdk import App, Environment
 import os
 
 load_dotenv()
+settings = dotenv_values(".env")
 
 default_account = os.getenv('CDK_DEFAULT_ACCOUNT')
 default_region = os.getenv('CDK_DEFAULT_REGION')
@@ -15,6 +16,6 @@ if not default_account or not default_region:
 env = Environment(account=default_account, region=default_region)
 app = App()
 
-VpcStack(app, "VpcStack", env=env)
+VpcStack(app, "VpcStack", env=env, settings=settings)
 
 app.synth()
--- aws/cdk-python/stacks/vpc_stack.py
+++ aws/cdk-python/stacks/vpc_stack.py
@@ -2,13 +2,34 @@ from aws_cdk import (
     Stack,
     aws_ec2
 )
-from aws_cdk.aws_ec2 import Vpc
 from constructs import Construct
+from aws_cdk.aws_ec2 import Vpc, SubnetType
+import os
 
 class VpcStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, settings: dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        self.vpc = Vpc(self, 'vpc', max_azs=2, vpc_name='test', ip_addresses=aws_ec2.IpAddresses.cidr("10.10.0.0/16"))
+        self.vpc = Vpc(
+            self, 
+            f'vpc-{os.getenv('PROJECT_NAME')}', 
+            vpc_name='test', 
+            ip_addresses=aws_ec2.IpAddresses.cidr(settings.get("VPC_CIDR", "10.0.0.0/16")), 
+            subnet_configuration=[
+                { 
+                    "cidrMask": 24, 
+                    "name": 'PublicSubnetA', 
+                    "subnetType": SubnetType.PUBLIC, 
+                },
+                { 
+                    "cidrMask": 24, 
+                    "name": 'PrivateSubnetA', 
+                    "subnetType": SubnetType.PRIVATE_WITH_EGRESS, 
+                },
+            ],
+            nat_gateways=0,
+            availability_zones=["eu-north-1a"],
+            restrict_default_security_group=False
+        )
 
